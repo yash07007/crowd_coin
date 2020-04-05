4e74e2b6992b4152d2a7adc940e46f6638c43594
Yash Solanki <yashsolanki777@gmail.com>
2020-04-06 00:47:48 +0530

    Index and NewCampaigns Implemented

---
+++ pages/campaigns/new.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { Form, Button, Input, Message } from "semantic-ui-react";
+import Layout from "../../components/Layout";
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
+
+class CampaignNew extends Component {
+    state = {
+        minimumContribution: "",
+        errorMessage: "",
+        loading: false
+    };
+
+    onSubmit = async event => {
+        event.preventDefault();
+        this.setState({ loading: true, errorMessage: "" });
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (error) {
+            this.setState({ errorMessage: error.message });
+        }
+        this.setState({ loading: false });
+    };
+
+    render() {
+        return (
+            <Layout>
+                <h3>Create a Campaign</h3>
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Field error={!!this.state.errorMessage}>
+                        <label>Minimum Contribution</label>
+                        <Input
+                            label="wei"
+                            labelPosition="right"
+                            value={this.state.minimumContribution}
+                            onChange={event =>
+                                this.setState({
+                                    minimumContribution: event.target.value
+                                })
+                            }
+                        />
+                    </Form.Field>
+                    <Message
+                        error
+                        header="Error!"
+                        content={this.state.errorMessage}
+                    />
+                    <Button loading={this.state.loading} primary>
+                        Create!
+                    </Button>
+                </Form>
+            </Layout>
+        );
+    }
+}
+
+export default CampaignNew;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory";
+import { Card, Button } from "semantic-ui-react";
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
-    async componentDidMount() {
+    static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
+        return { campaigns: campaigns };
+    }
+
+    renderCampaingns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaigns</a>,
+                fluid: true
+            };
+        });
+        return <Card.Group items={items}></Card.Group>;
     }
 
     render() {
-        return <div>Campaigns Index</div>;
+        return (
+            <Layout>
+                <div>
+                    <h3>Open Campaigns</h3>
+                    <Button
+                        floated="right"
+                        content="Create Campaign"
+                        icon="add"
+                        primary
+                    />
+                    {this.renderCampaingns()}
+                </div>
+            </Layout>
+        );
     }
 }
 
